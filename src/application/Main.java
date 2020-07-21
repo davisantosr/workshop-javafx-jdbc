919d5d63581abf8aa4b596d911ccc98e03a2fb20
DAVI SANTOS <davi-rocha2@hotmail.com>
2020-07-21 10:19:54 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,9 +8,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-	@Override
 
+
+public class Main extends Application {
+	
+	private static Scene mainScene;	
+	
+	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
@@ -19,13 +23,17 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
